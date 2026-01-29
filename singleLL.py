220131df6b5a0a2ad345f862d748c1d9cfac701a
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-29 21:20:35 +0530

    New linked list for deleting the data

--- singleLL.py
+++ singleLL.py
@@ -148,3 +148,62 @@ obj.printLL()
 
 
 
+# New linked list for deleting the data
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class SingleLL:
+    def __init__(self):
+        self.head = None
+
+    def insertAtTheEnd(self, value):
+        temp = Node(value)
+        if self.head is None:
+            self.head = temp
+        else:
+            t1 = self.head
+            while t1.next is not None:
+                t1 = t1.next
+            t1.next = temp   # linking the new node
+
+    def insertAtMid(self,value,x):
+        temp = Node(value)
+        t1 = self.head
+        while(t1.next != None):
+            if(t1.data == x):
+                temp.next = t1.next
+                t1.next = temp
+            t1 = t1.next
+
+    def deleteLL(self,value): # delete the value 
+        t1 = self.head
+        previous = t1
+        if (self.head == value):
+            self.head = t1.next
+        while (t1.next != None):
+            if(t1.data == value):
+                previous.next = t1.next
+                break
+            else:
+                previous = t1
+                t1 = t1.next
+        if(t1.data == value):
+            previous.next = None
+     
+    def printLL(self):
+        t1 = self.head
+        while t1 is not None:
+            print(t1.data)
+            t1 = t1.next
+
+
+obj = SingleLL()
+obj.insertAtTheEnd(10)
+obj.insertAtTheEnd(20)
+obj.insertAtTheEnd(30)
+# obj.insertAtMid(40,20)
+obj.deleteLL(10)
+obj.printLL()
