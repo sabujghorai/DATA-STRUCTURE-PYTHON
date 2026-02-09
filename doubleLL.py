2468e73d82eec6f3ba3a1bdf8ee7460fff9c834d
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-10 00:11:27 +0530

    commenting all

--- doubleLL.py
+++ doubleLL.py
@@ -1,36 +1,36 @@
-class Node:
-    def __init__(self,value = None):
-        self.data = value
-        self.next = None
-        self.prev = None
+# class Node:
+#     def __init__(self,value = None):
+#         self.data = value
+#         self.next = None
+#         self.prev = None
 
-class DoubleLL:
-    def __init__(self):
-        self.head = None
+# class DoubleLL:
+#     def __init__(self):
+#         self.head = None
 
-    def insertAtTheEnd(self,value):
-        temp = Node(value)
-        if(self.head == None):
-            self.head = temp
-            return
+#     def insertAtTheEnd(self,value):
+#         temp = Node(value)
+#         if(self.head == None):
+#             self.head = temp
+#             return
         
-        t = self.head
-        while(t.next != None):
-            t = t.next
+#         t = self.head
+#         while(t.next != None):
+#             t = t.next
 
-        t.next = temp
-        temp.prev = t
+#         t.next = temp
+#         temp.prev = t
 
-    def printDLL(self):
-        t = self.head
-        while(t.next != None):
-            print(t.data,end = " <--> ")
-            t = t.next
-        print(t.data)
+#     def printDLL(self):
+#         t = self.head
+#         while(t.next != None):
+#             print(t.data,end = " <--> ")
+#             t = t.next
+#         print(t.data)
 
-obj = DoubleLL()
-obj.insertAtTheEnd(10)
-obj.insertAtTheEnd(20)
-obj.insertAtTheEnd(30)
-obj.insertAtTheEnd(40)
-obj.printDLL()
+# obj = DoubleLL()
+# obj.insertAtTheEnd(10)
+# obj.insertAtTheEnd(20)
+# obj.insertAtTheEnd(30)
+# obj.insertAtTheEnd(40)
+# obj.printDLL()
