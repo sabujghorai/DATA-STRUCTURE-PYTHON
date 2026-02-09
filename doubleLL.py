e25028202cbaa74ae9a1ea76b598deb36cde11d6
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-09 23:45:56 +0530

    making a double linked list class

--- doubleLL.py
+++ doubleLL.py
@@ -3,4 +3,12 @@ class Node:
         self.data = value
         self.next = None
         self.prev = None
+
+class DoubleLL:
+    def __init__(self):
+        self.head = None
+
+    def insertAtTheEnd(self,value):
+        temp = Node(value)
         
+    
