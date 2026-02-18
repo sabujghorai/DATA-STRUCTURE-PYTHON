c410d181b8e7e365ce964add0324c6c91cbbc6f1
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-18 17:12:46 +0530

    adding a space inside the end method of line no 36

--- doubleLL.py
+++ doubleLL.py
@@ -33,7 +33,7 @@ class DoubleLL :
     def printDLL(self):
         t = self.head
         while(t.next != None):
-            print(t.data, end= "<-->")
+            print(t.data, end= " <--> ")
             t = t.next
         print(t.data)
 
