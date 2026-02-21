264b5d4d5410b240e7f3acb8b046f8091e252105
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-21 12:41:23 +0530

    making a method to see the top element of the stack

--- stack.py
+++ stack.py
@@ -4,3 +4,12 @@ class Stack :
     
     def length(self):
         return len(self.s)
+    
+    def push(self,value):
+        return self.s.insert(0,value)
+    
+    def peek(self):
+        if len(self.s) == 0:
+            raise Exception("The stack is empty")
+        else:
+            return self.s[0]
