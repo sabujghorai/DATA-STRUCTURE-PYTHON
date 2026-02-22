7f35d717a1907ea639662ba2b57d13472626ba53
sabujghorai <ghoraisabuj4@gmail.com>
2026-02-22 23:31:57 +0530

    commenting all

--- stack.py
+++ stack.py
@@ -1,30 +1,30 @@
-class Stack :
-    def __init__(self):
-        self.s = []
+# class Stack :
+#     def __init__(self):
+#         self.s = []
     
-    def length(self):
-        return len(self.s)
+#     def length(self):
+#         return len(self.s)
     
-    def push(self,value):
-        return self.s.insert(0,value)
+#     def push(self,value):
+#         return self.s.insert(0,value)
     
-    def peek(self):
-        if len(self.s) == 0:
-            raise Exception("The stack is empty")
-        else:
-            return self.s[0]
+#     def peek(self):
+#         if len(self.s) == 0:
+#             raise Exception("The stack is empty")
+#         else:
+#             return self.s[0]
         
-    def pop(self):
-        if len(self.s) == 0:
-            raise Exception("The stack is empty")
-        else:
-            return self.s.pop(0)
+#     def pop(self):
+#         if len(self.s) == 0:
+#             raise Exception("The stack is empty")
+#         else:
+#             return self.s.pop(0)
         
-stk = Stack()
-stk.push(10)
-stk.push(20)
-stk.push(30)
-stk.push(40)
-print(stk.pop()) # delete the top element and print it which is 40
-print(stk.pop()) # delete the top element and print it which is 30
-print(stk.peek()) # print the top element which is now 20
+# stk = Stack()
+# stk.push(10)
+# stk.push(20)
+# stk.push(30)
+# stk.push(40)
+# print(stk.pop()) # delete the top element and print it which is 40
+# print(stk.pop()) # delete the top element and print it which is 30
+# print(stk.peek()) # print the top element which is now 20
