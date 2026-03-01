a31b84a60dfcee9faf19538b325a452fd8574dda
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-01 16:51:53 +0530

    completing the code

--- queue.py
+++ queue.py
@@ -9,4 +9,19 @@ class Queue :
         if len(self.value) == 0 :
             print("The queue is empty")
         else:
-            return self.pop(0)
+            return self.value.pop(0)
+    
+    def isEmpty(self):
+        if len(self.value) == 0:
+            raise Exception("The queue is empty")
+
+Q = Queue()
+Q.insert(10)
+Q.insert(20)
+Q.insert(30)
+Q.insert(40)
+print(Q.delete())
+print(Q.delete())
+print(Q.delete())
+print(Q.delete())
+print(Q.delete())
