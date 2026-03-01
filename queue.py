712656240e705f3dc07e7a093ca0e565f44ec16d
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-01 16:39:48 +0530

    making a class of queue

--- queue.py
+++ queue.py
@@ -1,23 +1,3 @@
-class Queue:
+class Queue :
     def __init__(self):
-        self.items = []
-
-    def isEmpty(self):
-        return len(self.items) == 0
-    
-    def insert(self,values):
-        self.items.append(values)
-
-    def delete(self):
-        if (self.isEmpty()):
-            print("The queue is empty")
-        else:
-            return self.items.pop(0)
-q = Queue()
-q.insert(10)
-q.insert(20)
-q.insert(30)
-print(q.delete()) # delete the first element --> 10
-print(q.delete()) # delete the second element --> 20
-print(q.delete()) # delete the third element --> 30
-q.delete() # prints the queue is empty f
+        self.value = []
