a8ef76dc6f2e8421fde6e2688e26987ea9195214
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-03 09:33:47 +0530

    writing a comment at line no 7

--- circularQueue.py
+++ circularQueue.py
@@ -4,7 +4,7 @@ class CircularQueue:
         self.item = [None]*size
         self.front = self.real = -1
 
-    def Enqueue(self,value):
+    def Enqueue(self,value): # code for insertion
         if ((self.rear + 1)% self.front ):
             print("The queue is full")
         
