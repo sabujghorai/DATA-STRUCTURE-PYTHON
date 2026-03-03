6ce3e62b65d07b94da2147ef539be4c06254067d
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-03 10:06:43 +0530

    testing the code

--- circularQueue.py
+++ circularQueue.py
@@ -2,10 +2,10 @@ class CircularQueue:
     def __init__(self,size):
         self.size = size
         self.item = [None]*size
-        self.front = self.real = -1
+        self.front = self.rear = -1
 
-    def Enqueue(self,value): # code for insertion
-        if ((self.rear + 1)% self.front ):
+    def enqueue(self,value): # code for insertion
+        if ((self.rear + 1) % self.size == self.front ):
             print("The queue is full")
         elif self.front == -1 : # checks the queue is empty ot not
             self.front = self.rear = 0
@@ -14,7 +14,7 @@ class CircularQueue:
             self.rear = (self.rear +1) % self.size
             self.item[self.rear] = value
 
-    def Dequeue(self):
+    def dequeue(self):
         if (self.front == -1): # checks the queue is empty or not
             print("the Queue is Empty")
         elif self.front == self.rear: # means the queue has only one element
@@ -24,3 +24,9 @@ class CircularQueue:
              print(self.item[self.front])
              self.front = (self.front + 1)% self.size
 
+CQ = CircularQueue(4)
+CQ.enqueue(10)
+CQ.enqueue(20)
+CQ.enqueue(30)
+CQ.enqueue(40)
+CQ.enqueue(50)
