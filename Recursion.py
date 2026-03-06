5204f8b97dbbed3da57a38687942e382067799af
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-06 22:28:07 +0530

    commenting all

--- Recursion.py
+++ Recursion.py
@@ -1,17 +1,18 @@
-# Write a program using recursion to print the factorial of a number n take input from the user
-def factorial(n):
-    if(n == 0 or n == 1):
-        return 1
-    return n * factorial(n-1)
+# # Write a program using recursion to print the factorial of a number n take input from the user
+# def factorial(n):
+#     if(n == 0 or n == 1):
+#         return 1
+#     return n * factorial(n-1)
 
-b = int(input("Enter a number :"))
-print("The factorial of your number is : ", factorial(b))
+# b = int(input("Enter a number :"))
+# print("The factorial of your number is : ", factorial(b))
 
-# Write a program to print the fibonacci series using recursion
-def fibonacci(n):
-    if(n == 1 or n ==2):
-        return 1
-    return fibonacci(n-1)+fibonacci(n-2)
+# # Write a program to print the fibonacci series using recursion
+# def fibonacci(n):
+#     if(n == 1 or n ==2):
+#         return 1
+#     return fibonacci(n-1)+fibonacci(n-2)
 
-c = int(input("Enter a number for your fibonacci series :"))
-print("The fibonacci term is :" , fibonacci(c))
+# c = int(input("Enter a number for your fibonacci series :"))
+# print("The fibonacci term is :" , fibonacci(c))
+
