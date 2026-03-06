c6f6960c372ec5066b081768936b4bc6bb8d6a83
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-06 21:14:37 +0530

    clearning all the codes

--- Recursion.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter a number : "))
-fact = 1
-if a < 0:
-    print("Factorial doesn't exist for negative numbers.")
-else:
-    for i in range(1, a + 1):
-        fact = fact * i
-    print("The factorial is:", fact)
-    
