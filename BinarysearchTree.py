12bb706452d19e02efa4fb2b582eea5a38b49520
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-12 21:48:52 +0530

    nothing is changed

--- BinarysearchTree.py
+++ BinarysearchTree.py
@@ -4,22 +4,22 @@ class Node :
         self.right = None
         self.data = value
         
-    def insert(root,value):
-        if (root == None):
-            return Node(value)
-        if (root.data == None):
-            return root
-        if(root.data > value):
-            root.right = insert(root.left,value)
-        else:
-            root.left = insert(root.right,value)
+def insert(root,value):
+    if (root == None):
+        return Node(value)
+    if (root.data == None):
         return root
+    if(root.data > value):
+        root.right = insert(root.left,value)
+    else:
+        root.left = insert(root.right,value)
+    return root
 
-    def Inorder(root,value):
-        if (root != None):
-            Inorder(root.left)
-            print(root.data, end = " ")
-            Inorder(root.right)
+def Inorder(root,value):
+    if (root != None):
+        Inorder(root.left)
+        print(root.data, end = " ")
+        Inorder(root.right)
 
 root = Node(20)
 root.left = Node(15)
