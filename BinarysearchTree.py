40e3e1029d59cf5757ae642e0f976f3c9268a55f
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-12 21:58:30 +0530

    implementing the searching code

--- BinarysearchTree.py
+++ BinarysearchTree.py
@@ -15,6 +15,18 @@ def insert(root,value):
         root.right = insert(root.right,value)
     return root
 
+def searching(root,value):
+    if (root == None):
+        return Node(value)
+    if (root.data == None):
+        return root
+    if(root.data > value):
+        root.left = insert(root.left,value)
+    else:
+        root.right = insert(root.right,value)
+    return root
+
+
 def Inorder(root):
     if (root != None):
         Inorder(root.left)
