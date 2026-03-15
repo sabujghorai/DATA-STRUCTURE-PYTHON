71a41e66c5e9e1386e3f7a5f173c49c0ea670a21
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-15 12:12:34 +0530

    making  function to print the length of the node

--- TreePractice.py
+++ TreePractice.py
@@ -4,4 +4,11 @@ class Node:
         self.left = None
         self.right = None
         self.data = value
-        
+
+def count_Node(root):
+    if(root == None):
+        return 0
+    count_left = count_Node(root.left)
+    count_right = count_Node(root.right)
+
+    return 1+count_left+count_right
