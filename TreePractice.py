52aeb192c96ac5815bd75e156d2e5d8296db08c8
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-15 21:59:34 +0530

    writing the perfect code

--- TreePractice.py
+++ TreePractice.py
@@ -1,52 +1,52 @@
-# Write a function that returns the number of nodes in a binary tree.
-class Node:
-    def __init__(self,value):
-        self.left = None
-        self.right = None
-        self.data = value
+# # Write a function that returns the number of nodes in a binary tree.
+# class Node:
+#     def __init__(self,value):
+#         self.left = None
+#         self.right = None
+#         self.data = value
 
-def count_Node(root):
-    if(root == None):
-        return 0
-    count_left = count_Node(root.left)
-    count_right = count_Node(root.right)
+# def count_Node(root):
+#     if(root == None):
+#         return 0
+#     count_left = count_Node(root.left)
+#     count_right = count_Node(root.right)
 
-    return 1+count_left+count_right
+#     return 1+count_left+count_right
 
-root = Node(50)
-root.left = Node(40)
-root.right = Node(60)
-root.left.left = Node(30)
-root.left.right = Node(45)
-root.right.left = Node(55)
-root.right.right = Node(70)
+# root = Node(50)
+# root.left = Node(40)
+# root.right = Node(60)
+# root.left.left = Node(30)
+# root.left.right = Node(45)
+# root.right.left = Node(55)
+# root.right.right = Node(70)
 
-print("The Number of Node is :",count_Node(root))
+# print("The Number of Node is :",count_Node(root))
 
-# Find the Height of a Binary Tree
-class Node:
-    def __init__(self,value):
-        self.left = None
-        self.right = None
-        self.data = value
+# # Find the Height of a Binary Tree
+# class Node:
+#     def __init__(self,value):
+#         self.left = None
+#         self.right = None
+#         self.data = value
 
-def Height(root):
-    if(root == None):
-        return 0
-    left_Height = Height(root.left)
-    right_Height = Height(root.right)
+# def Height(root):
+#     if(root == None):
+#         return 0
+#     left_Height = Height(root.left)
+#     right_Height = Height(root.right)
 
-    return 1+max(left_Height,right_Height)
+#     return 1+max(left_Height,right_Height)
 
-root = Node(70)
-root.left = Node(40)
-root.right = Node(80)
-root.left.left = Node(30)
-root.left.right = Node(50)
-root.right.left = Node(75)
-root.right.right = Node(90)
+# root = Node(70)
+# root.left = Node(40)
+# root.right = Node(80)
+# root.left.left = Node(30)
+# root.left.right = Node(50)
+# root.right.left = Node(75)
+# root.right.right = Node(90)
 
-print("The Height of the tree is :",Height(root))
+# print("The Height of the tree is :",Height(root))
 
 # Write a function that returns the number of leaf nodes (nodes with no children).
 
@@ -57,6 +57,19 @@ class Node:
         self.data = value
 
 def Leaf_count(root):
+    if(root == None):
+        return 0
     if(root.left == None and root.right == None):
         return 1
     
+    return Leaf_count(root.left)+Leaf_count(root.right)
+
+root = Node(70)
+root.left = Node(40)
+root.right = Node(80)
+root.left.left = Node(30)
+root.left.right = Node(50)
+root.right.left = Node(75)
+root.right.right = Node(90)
+
+print("Total leaf is :",Leaf_count(root))
