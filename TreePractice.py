1272c2bfcd4ea4b90b98e2644d54281296e6dc0c
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-15 12:03:52 +0530

    making a class of Node

--- TreePractice.py
+++ TreePractice.py
@@ -1,1 +1,7 @@
 # Write a function that returns the number of nodes in a binary tree.
+class Node:
+    def __init__(self,value):
+        self.left = None
+        self.right = None
+        self.data = value
+        
