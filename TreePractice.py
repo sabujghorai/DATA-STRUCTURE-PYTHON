44a7a50b5df8161ec30ee616cef43361ff571cd0
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-15 21:48:29 +0530

    making a class node

--- TreePractice.py
+++ TreePractice.py
@@ -49,3 +49,10 @@ root.right.right = Node(90)
 print("The Height of the tree is :",Height(root))
 
 # Write a function that returns the number of leaf nodes (nodes with no children).
+
+class Node:
+    def __init__(self,value):
+        self.left = None
+        self.right = None
+        self.data = value
+        
