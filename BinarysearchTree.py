c1705e1cc7fe386dc58c0017066b4a5ee1a81fca
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-17 22:09:27 +0530

    updated the code

--- BinarysearchTree.py
+++ BinarysearchTree.py
@@ -1,72 +1,88 @@
-class Node :
-    def __init__(self,value):
+class Node:
+    def __init__(self, value):
         self.left = None
         self.right = None
         self.data = value
-        
-def insert(root,value):
-    if (root == None):
+
+
+def insert(root, value):
+    if root is None:
         return Node(value)
-    if (root.data == None):
-        return root
-    if(root.data > value):
-        root.left = insert(root.left,value)
+
+    if root.data > value:
+        root.left = insert(root.left, value)
     else:
-        root.right = insert(root.right,value)
+        root.right = insert(root.right, value)
+
     return root
 
-def searching(root,value):
-    if (root == None):
-        print("Element not found..", end = "\n")
+
+def searching(root, value):
+    if root is None:
+        print("Element not found..")
         return
-    if (root.data == value):
-        print("Element found..",end = "\n")
+
+    if root.data == value:
+        print("Element found..")
         return
-    if(root.data > value):
-        searching(root.left,value)
+
+    if root.data > value:
+        searching(root.left, value)
     else:
-        searching(root.right,value)
+        searching(root.right, value)
+
+
 def get_successor(root):
     root = root.right
-    while(root != None and root.left != None):
+    while root and root.left:
         root = root.left
     return root
 
-def delete(root,value):
-    if(root == None):
-        return root
-    if(root.data > value):
-        root.left = delete(root.left , value)
-    elif(root.data < value):
-        root.right = delete(root.right , value)
 
+def delete(root, value):
+    if root is None:
+        return root
+
+    if root.data > value:
+        root.left = delete(root.left, value)
+    elif root.data < value:
+        root.right = delete(root.right, value)
     else:
-        if (root.left == None):
+        # Node found
+        if root.left is None:
             return root.right
-        if(root.right == None):
+        if root.right is None:
             return root.left
-        else:
-            succ = get_successor(root)
-            root.data = succ.data
-            root.right = delete(root.right,succ.data)
+
+        # Node with 2 children
+        succ = get_successor(root)
+        root.data = succ.data
+        root.right = delete(root.right, succ.data)
+
     return root
 
+
 def Inorder(root):
-    if (root != None):
-        Inorder(root.left) 
-        print(root.data, end = " ")
+    if root:
+        Inorder(root.left)
+        print(root.data, end=" ")
         Inorder(root.right)
 
-root = insert(None,20)
-root = insert(root,15)
-root = insert(root,30)
-root = insert(root,40)
-root = insert(root,12)
-root = insert(root,18)
-root = insert(root,58)
-Inorder(root) # this inorder function arrange the element in increasing order
-print("\n")
-delete(root,12)
-print("\n")
+
+# Driver Code
+root = insert(None, 20)
+root = insert(root, 15)
+root = insert(root, 30)
+root = insert(root, 40)
+root = insert(root, 12)
+root = insert(root, 18)
+root = insert(root, 58)
+
+print("Original Tree:")
 Inorder(root)
-delete(root,40)
+print("\n")
+root = delete(root, 12)
+Inorder(root)
+print("\n")
+root = delete(root, 40)
+Inorder(root)
