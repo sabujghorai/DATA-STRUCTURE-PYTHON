d9becb2ae2752d79d87dc2f1d8beb162a57900fe
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-18 21:48:24 +0530

    finishing the if and else conditon

--- Graph.py
+++ Graph.py
@@ -7,4 +7,6 @@ class Graph:
         if(self.size > source >= 0 and self.size > destination >= 0):
             self.matrix[source][destination] = 1
             self.matrix[destination][source] = 1
-            
+        else:
+            print("The Edge you have entered is invalid..")
+    
