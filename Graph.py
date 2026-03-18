d6efec0bb833c2fbd0d0c093d25b3469ab13d770
sabujghorai <ghoraisabuj4@gmail.com>
2026-03-18 21:47:12 +0530

    writing the if condition

--- Graph.py
+++ Graph.py
@@ -2,3 +2,9 @@ class Graph:
     def __init__(self,vertex):
         self.matrix = [[0]*vertex for i in range(vertex)]
         self.size = vertex
+    
+    def add_edge(self,source,destination):
+        if(self.size > source >= 0 and self.size > destination >= 0):
+            self.matrix[source][destination] = 1
+            self.matrix[destination][source] = 1
+            
