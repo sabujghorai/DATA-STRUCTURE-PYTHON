2d2a00d7f92bccacaf1c74a6aca40af006934471
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-03-20 12:03:58 +0530

    making a function for add vertex

--- AdjacencyList.py
+++ AdjacencyList.py
@@ -1,3 +1,7 @@
 class Graph:
     def __init__(self):
         self.adjacencyList = {}
+
+    def add_vertex(self,vertex):
+        if vertex not in self.adjacencyList:
+            self.adjacencyList[vertex] = []
