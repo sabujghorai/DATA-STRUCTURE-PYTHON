6eaca9fe9787e00e8a8337b3afb9f5fb12e56087
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-03-22 11:54:28 +0530

    making a function of add egde

--- DFS.py
+++ DFS.py
@@ -2,3 +2,8 @@ class Graph :
     def __init__(self,vertex):
         self.matrix = [[0]*vertex for i in range(vertex)]
         self.size = vertex
+
+    def add_edge(self,source,destination):
+        if(0<=source<self.size and 0<=destination<self.size):
+            self.matrix[source][destination]
+            self.matrix[destination][source]
