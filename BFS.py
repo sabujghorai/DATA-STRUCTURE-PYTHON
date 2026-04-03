caa970300bbd2500bcf2cc2f30e249cb82555a79
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-03 11:10:19 +0530

    implementing a method for add egdes

--- BFS.py
+++ BFS.py
@@ -2,3 +2,9 @@ class Graph :
     def __init__(self,vertex):
         self.matrix = [[0]*vertex for x in range(vertex)]
         self.size = vertex
+    
+    def add_edge(self,source,destination):
+        if(0<=source<self.size and 0<=destination<self.size):
+            self.matrix = [source][destination] = 1
+            self.matrix = [destination][source] = 1
+            
