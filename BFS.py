8d0a708085aef24c8fc60a2c14eb179b7eb1312b
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-03 12:09:58 +0530

    implementing the BFS method

--- BFS.py
+++ BFS.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Graph :
     def __init__(self,vertex):
         self.matrix = [[0]*vertex for x in range(vertex)]
@@ -10,4 +12,11 @@ class Graph :
         else:
             print("Invalid edge")
     
-    
+    def BSF(self,source):
+        visited = [False] * self.size
+        queue = deque([source])
+        visited[source] = True
+
+        while(queue): # means queue is not equal to None
+            v = queue.popleft
+            print(v,end = " ")
