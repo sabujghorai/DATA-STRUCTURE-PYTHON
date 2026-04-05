9d9cbf937750601cf553b0cf1a3beafe483f2072
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-05 12:15:36 +0530

    updated the bubblesort file

--- Bubblesort.py
+++ Bubblesort.py
@@ -5,3 +5,7 @@ def BubbleSort(a):
         for j in range(0,n-1-i):
             if(a[j] > a[j+1]):
                 a[j],a[j+1] = a[j+1],a[j]
+
+a = [75,45,34,23,54,27,87,12,47,99]
+BubbleSort(a)
+print(a)
