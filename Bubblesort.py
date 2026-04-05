2ad30ecc8b95d1eb6423d179e467a4f36f302d73
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-05 12:25:51 +0530

    updated the bubbledort file

--- Bubblesort.py
+++ Bubblesort.py
@@ -13,3 +13,14 @@ print(a)
 
 
 # Modify bubble sort to sort the array in descending order.
+def Bubblesort(b):
+    k = len(b)
+
+    for i in range(k):
+        for j in range(0,k-1-i):
+            if(b[j] < b[j+1]):
+                b[j],b[j+1] = b[j+1],b[j]
+
+b = [11,22,33,44,55,66,77]
+Bubblesort(b)
+print(b)
