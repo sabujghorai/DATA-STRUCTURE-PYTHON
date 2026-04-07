f00bb7d45159b71d541af34ecafd67a8a340720a
Sabuj Ghorai <ghoraisabuj4@gmail.com>
2026-04-07 19:03:32 +0530

    updated the bubbleshort file

--- Bubblesort.py
+++ Bubblesort.py
@@ -4,10 +4,10 @@ def BubbleSort(a):
 
     for i in range(n):
         for j in range(0,n-1-i):
-            if(a[j] > a[j+1]):
+            if(a[j]>a[j+1]):
                 a[j],a[j+1] = a[j+1],a[j]
 
-a = [75,45,34,23,54,27,87,12,47,99]
+a = [3,4,5,2,9,7,6,12]
 BubbleSort(a)
 print(a)
 
@@ -56,3 +56,4 @@ def BubbleSort(d):
 d = ["zoo","xmas","string","list","list","tuple","alpha","beta","camel"]
 BubbleSort(d)
 print(d)
+
