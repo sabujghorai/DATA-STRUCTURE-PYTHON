1a99ff73e47cbb9378ebb40145fb1e6cb37e2e33
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-17 21:29:26 +0530

    Implementing the Stack code

--- Revision.py
+++ Revision.py
@@ -4,3 +4,34 @@ values = array("i",[1,2,3,4,5,7,6,9,8])
 for i in range(0,len(values)):
     print(values[i],end=" ")
 
+# stack 
+class Stack :
+    def __init__(self):
+        self.s = []
+    
+    def length(self):
+        return len(self.s)
+    
+    def push(self,value):
+        return self.s.insert(0,value)
+    
+    def peek(self):
+        if len(self.s) == 0:
+            raise Exception("The stack is empty")
+        else:
+            return self.s[0]
+        
+    def pop(self):
+        if len(self.s) == 0:
+            raise Exception("The stack is empty")
+        else:
+            return self.s.pop(0)
+        
+stk = Stack()
+stk.push(10)
+stk.push(20)
+stk.push(30)
+stk.push(40)
+print(stk.pop()) # delete the top element and print it which is 40
+print(stk.pop()) # delete the top element and print it which is 30
+print(stk.peek()) # print the top element which is now 20
