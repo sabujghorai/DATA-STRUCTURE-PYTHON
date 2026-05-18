9852da31f61bd093115ae75916d91c5b58100067
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-18 21:33:02 +0530

    created a class of double liked list

--- Revision.py
+++ Revision.py
@@ -162,3 +162,21 @@ class Node :
         self.next = None
         self.prev = None
 
+class DoubleLL :
+    def __init__(self):
+        self.head = None
+
+    def insertAtTheEnd(self,value):
+        temp = Node(value)
+        if(self.head == None):
+            self.head = temp
+            return
+        
+        t = self.head 
+        while(t.next != None):
+            t = t.next
+
+        t.next = temp
+        temp.prev = t
+
+    
