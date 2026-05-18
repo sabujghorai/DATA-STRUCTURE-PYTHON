c888a97a17bb66a2d580d22ddccfd7f577f4f2aa
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-18 21:21:02 +0530

    updated the revision file

--- Revision.py
+++ Revision.py
@@ -1,97 +1,97 @@
-# # Array
-# from array import *
-# values = array("i",[1,2,3,4,5,7,6,9,8])
-# for i in range(0,len(values)):
-#     print(values[i],end=" ")
+# Array
+from array import *
+values = array("i",[1,2,3,4,5,7,6,9,8])
+for i in range(0,len(values)):
+    print(values[i],end=" ")
 
-# # stack 
-# class Stack :
-#     def __init__(self):
-#         self.s = []
+# stack 
+class Stack :
+    def __init__(self):
+        self.s = []
     
-#     def length(self):
-#         return len(self.s)
+    def length(self):
+        return len(self.s)
     
-#     def push(self,value):
-#         return self.s.insert(0,value)
+    def push(self,value):
+        return self.s.insert(0,value)
     
-#     def peek(self):
-#         if len(self.s) == 0:
-#             raise Exception("The stack is empty")
-#         else:
-#             return self.s[0]
+    def peek(self):
+        if len(self.s) == 0:
+            raise Exception("The stack is empty")
+        else:
+            return self.s[0]
         
-#     def pop(self):
-#         if len(self.s) == 0:
-#             raise Exception("The stack is empty")
-#         else:
-#             return self.s.pop(0)
+    def pop(self):
+        if len(self.s) == 0:
+            raise Exception("The stack is empty")
+        else:
+            return self.s.pop(0)
         
-# stk = Stack()
-# stk.push(10)
-# stk.push(20)
-# stk.push(30)
-# stk.push(40)
-# print(stk.pop()) # delete the top element and print it which is 40
-# print(stk.pop()) # delete the top element and print it which is 30
-# print(stk.peek()) # print the top element which is now 20
+stk = Stack()
+stk.push(10)
+stk.push(20)
+stk.push(30)
+stk.push(40)
+print(stk.pop()) # delete the top element and print it which is 40
+print(stk.pop()) # delete the top element and print it which is 30
+print(stk.peek()) # print the top element which is now 20
 
 
-# Stack = []
-# Stack.append(10)
-# Stack.append(20)
-# Stack.append(30)
-# Stack.append(40)
-# print(Stack)
+Stack = []
+Stack.append(10)
+Stack.append(20)
+Stack.append(30)
+Stack.append(40)
+print(Stack)
 
-# Stack.pop()
-# Stack.pop()
-# print(Stack)
-
-
-# # Queue
-# class Queue:
-
-#     def __init__(self):
-#         self.q = []
-
-#     def enqueue(self,value):
-#         self.q.append(value)
-
-#     def dequeue(self):
-#         return self.q.pop(0)
-
-#     def peek(self):
-#         return self.q[0]
-
-# q = Queue()
-
-# q.enqueue(10)
-# q.enqueue(20)
-# q.enqueue(30)
-
-# print(q.dequeue())
-# print(q.peek())
+Stack.pop()
+Stack.pop()
+print(Stack)
 
 
-# # Recursion 
+# Queue
+class Queue:
 
-# # # write a program to find the sum of n natural number
-# def sum(a):
-#     if(a == 0):
-#         return 0
-#     return a+sum(a-1)
+    def __init__(self):
+        self.q = []
 
-# a = int(input("Enter the number of turms :"))
-# print("The sum of your n number of term is :",sum(a))
+    def enqueue(self,value):
+        self.q.append(value)
 
-# # Write a program using recursion to print the factorial of a number n take input from the user
-# def factorial(b):
-#     if(b == 0):
-#         return 1
-#     return b*(factorial(b-1))
-# b = int(input("Enter the number you want to find factorial :"))
-# print("The factorial of your number is : ", factorial(b))
+    def dequeue(self):
+        return self.q.pop(0)
+
+    def peek(self):
+        return self.q[0]
+
+q = Queue()
+
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+
+print(q.dequeue())
+print(q.peek())
+
+
+# Recursion 
+
+# # write a program to find the sum of n natural number
+def sum(a):
+    if(a == 0):
+        return 0
+    return a+sum(a-1)
+
+a = int(input("Enter the number of turms :"))
+print("The sum of your n number of term is :",sum(a))
+
+# Write a program using recursion to print the factorial of a number n take input from the user
+def factorial(b):
+    if(b == 0):
+        return 1
+    return b*(factorial(b-1))
+b = int(input("Enter the number you want to find factorial :"))
+print("The factorial of your number is : ", factorial(b))
 
 
 
@@ -147,7 +147,7 @@ class SingleLL :
 
 obj = SingleLL()
 obj.insertAtEnd(10)
-obj.insertAtEnd(90)
+obj.insertAtEnd(20)
 obj.insertAtEnd(30)
 obj.insertAtEnd(40)
 obj.insertAtEnd(50)
