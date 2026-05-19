2bdd8725107137a9a979b40bd77c003993b4f956
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-19 20:43:11 +0530

    attatched leaf

--- Revision.py
+++ Revision.py
@@ -264,3 +264,12 @@ def postOrder(root):
         postOrder(root.left)
         postOrder(root.right)
         print(root.data , end = " ")
+
+# Tree
+root = Node(1)
+root.left = Node(3)
+root.right = Node(5)
+root.left.left = Node(2)
+root.left.right = Node(4)
+root.right.right = Node(8)
+
