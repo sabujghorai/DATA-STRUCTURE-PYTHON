f02791d21b040f253aa926ebe5c95b72a20dba78
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-20 20:56:11 +0530

    implementing the function to add edges

--- Revision.py
+++ Revision.py
@@ -290,4 +290,9 @@ class Graph:
         self.matrix = [[0]*vertex for i in range(vertex)]
         self.size = vertex
     
-    
+    def add_edge(self,source,destination):
+        if(self.size > source >= 0 and self.size > destination >= 0):
+            self.matrix[source][destination] = 1
+            self.matrix[destination][source] = 1
+        else:
+            print("The Edge you have entered is invalid..")
