e89f7a2cbc093d1bcffe437a8ce52ef5ab9731d8
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-20 21:00:34 +0530

    implementing the condition to print the bubble sort

--- Revision.py
+++ Revision.py
@@ -317,4 +317,8 @@ G.print_graph()
 def BubbleSort(a):
     n = len(a)
 
-   
+    for i in range(n):
+        for j in range(0,n-1-i):
+            if(a[j]>a[j+1]):
+                a[j],a[j+1] = a[j+1],a[j]
+
