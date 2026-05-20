7b8393d13cdbdcb87239a26330b8fc37ca02f200
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-20 20:59:55 +0530

    implementing the bubblesort code

--- Revision.py
+++ Revision.py
@@ -317,11 +317,4 @@ G.print_graph()
 def BubbleSort(a):
     n = len(a)
 
-    for i in range(n):
-        for j in range(0,n-1-i):
-            if(a[j]>a[j+1]):
-                a[j],a[j+1] = a[j+1],a[j]
-
-a = [3,4,5,2,9,7,6,12]
-BubbleSort(a)
-print(a)
+   
