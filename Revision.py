0f971c3d2c20506bd505e9c116917c08926ab5c6
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-20 20:55:21 +0530

    implementing the graph code

--- Revision.py
+++ Revision.py
@@ -290,23 +290,4 @@ class Graph:
         self.matrix = [[0]*vertex for i in range(vertex)]
         self.size = vertex
     
-    def add_edge(self,source,destination):
-        if(self.size > source >= 0 and self.size > destination >= 0):
-            self.matrix[source][destination] = 1
-            self.matrix[destination][source] = 1
-        else:
-            print("The Edge you have entered is invalid..")
-
-    def print_graph(self):
-        for row in self.matrix:
-            print(' '.join(map(str,row)))
-
-G = Graph(5)
-G.add_edge(0,2)
-G.add_edge(0,1)
-G.add_edge(1,3)
-G.add_edge(2,3)
-G.add_edge(3,4)
-G.add_edge(2,4)
-
-G.print_graph()
+    
