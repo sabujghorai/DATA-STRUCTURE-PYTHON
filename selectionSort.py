09260e5c401874ae2b50f886a2081085b6a1e474
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-22 18:40:02 +0530

    implementing the code for selection sort

--- selectionSort.py
+++ selectionSort.py
@@ -1,5 +1,10 @@
 def selectionSort(a):
     n = len(a)
 
-    # for i in range (n):
-    #     for j in range (0:)
+    for i in range (n):
+        min = i
+        for j in range (i , n):
+            if(a[min]>a[j]):
+                min = j
+        a[i],a[min] = a[min],a[i]
+        
