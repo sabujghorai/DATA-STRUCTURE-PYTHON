6a898c6501b5eaca3eebbc868e192d091d5d9173
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-22 18:52:20 +0530

    updated the selection sort file

--- selectionSort.py
+++ selectionSort.py
@@ -14,3 +14,12 @@ print(a)
 
 
 # sort the elements arr = [64,25,12,22,11] in ascending order
+def ascendingOrderSort(a):
+    n = len(a)
+
+    for i in range(n):
+        min = i
+        for j in range(i,n):
+            if(a[min]>a[j]):
+                min  = j
+                
