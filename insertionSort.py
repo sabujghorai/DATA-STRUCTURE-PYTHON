c9eb2ce06d1c2021df6c459b81ee8008faf5c67a
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-23 23:11:04 +0530

    implementing the while loop

--- insertionSort.py
+++ insertionSort.py
@@ -22,3 +22,7 @@ def insertionSortDescending(arr):
     for i in range(1, n):
         key = arr[i]
         j = i - 1
+
+        while j >= 0 and arr[j] < key:
+            arr[j + 1] = arr[j]
+            j -= 1
