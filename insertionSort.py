5d1cfa645a5534286751dec57a5d3022335ec36a
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-24 20:56:34 +0530

    updated the insertion sort file

--- insertionSort.py
+++ insertionSort.py
@@ -2,10 +2,10 @@ def InsertionSort(a):
     n = len(a)
 
     for i in range(1,n):
-        key = a[1]
+        key = a[i]
         j = i-1
 
-        while (j <= 0 and key < a[j]):
+        while (j >= 0 and key < a[j]):
             a[j+1] = a[j]
             j = j-1
 
