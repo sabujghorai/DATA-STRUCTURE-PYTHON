9595ec4da7f17256b09d0732342a9f6ebeecdf0c
sabujghorai <ghoraisabuj4@gmail.com>
2026-05-26 11:58:59 +0530

    updated the merge sort file

--- mergeSort.py
+++ mergeSort.py
@@ -1,6 +1,11 @@
 def Divide(arr,l,r):
     if(l < r):
         m = (l+r) // 2
-        Divide(l , m)
-        Divide(m+1 , r)
-        
+        Divide(l , m)#   ------------|
+        Divide(m+1 , r)  # ----------|----|
+        Merge(arr , l , m , r)  #    |    |
+                               #     |    |
+def Merge(arr , l , m , r):    #     |    |
+    s1 = m - l +1  # ________________|    |
+    s2 =  r - m    # _____________________|
+    
