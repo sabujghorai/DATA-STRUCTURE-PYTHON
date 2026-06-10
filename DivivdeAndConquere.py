dc9be805dc08147ce13dfaf3fb239d6e4cf67ab4
sabujghorai <ghoraisabuj4@gmail.com>
2026-06-10 19:34:01 +0530

    updated the divide and conquere file

--- DivivdeAndConquere.py
+++ DivivdeAndConquere.py
@@ -1,4 +1,9 @@
 def find_min_max(arr ,start , end):
     if(start == end ):
         return arr[start] , arr[end]
+
+    if(start+1 == end):
+        if(arr[start]<arr[end]):
+            return arr[start] , arr[end]
+        
         
