6afe7df21a6b1b9cdac567782dfb7f6fee0c4afc
sabujghorai <ghoraisabuj4@gmail.com>
2026-06-10 19:58:36 +0530

    updated the divide and conquere file

--- DivivdeAndConquere.py
+++ DivivdeAndConquere.py
@@ -8,10 +8,14 @@ def find_min_max(arr ,start , end):
         else:
             return arr[end] , arr[start]
         
-        mid = (start+end) // 2
+    mid = (start+end) // 2
 
-        min1 , max1 = find_min_max(arr , start , mid)
-        min2 , max2 = find_min_max(arr , mid+1 , end)
+    min1 , max1 = find_min_max(arr , start , mid)
+    min2 , max2 = find_min_max(arr , mid+1 , end)
 
-        return min(min1,min2) , max(max1,max2)
+    return min(min1,min2) , max(max1,max2)
     
+arr = [28,12,4,23,53,2,34,87,100]
+min , max = find_min_max(arr , 0 , len(arr) -1)
+print("The minimum number is : ", min)
+print("The Maximum number is :", max)
