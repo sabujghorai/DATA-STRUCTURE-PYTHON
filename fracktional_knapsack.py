44514f4863985ed5eb9cd2ef90d50cc4da280ccd
sabujghorai <ghoraisabuj4@gmail.com>
2026-06-22 20:44:45 +0530

    updated the knapsack problem

--- fracktional_knapsack.py
+++ fracktional_knapsack.py
@@ -1,2 +1,6 @@
 def fractional_knapsack(item_weight , price , knapsack_weight):
     n = len(item_weight)
+
+    items = [(item_weight[i],price[i],price[i]/item_weight[i]) for i in range(n)] 
+    # Creates a list of tuples containing (weight, price, price-to-weight ratio)
+    #  for every item from index 0 to n-1
