2fe60183c874bcdc3a1c77728ddca98ffdbc1f95
sabujghorai <ghoraisabuj4@gmail.com>
2026-06-23 21:23:48 +0530

    chaged the parameter n to items

--- Dynamic_Programming.py
+++ Dynamic_Programming.py
@@ -1,4 +1,4 @@
-def knapsack_recursive(weights , values , knapsack_weight , n): # n is the items here
-    if(n == 0 or knapsack_weight == 0):
+def knapsack_recursive(weights , values , knapsack_weight , items): # n is the items here
+    if(items == 0 or knapsack_weight == 0):
         return 0
     
