4c3a8bf81dc0879c1c0456b3e6b6ea7809f36480
sabujghorai <ghoraisabuj4@gmail.com>
2026-07-21 23:12:49 +0530

    updated the factional knapsack problem

--- DSA 📈/fracktional_knapsack.py
+++ DSA 📈/fracktional_knapsack.py
@@ -4,7 +4,6 @@ def fractional_knapsack(item_weight , price , knapsack_weight):
     items = [(item_weight[i],price[i],price[i]/item_weight[i]) for i in range(n)] 
     # Creates a list of tuples containing (weight, price, price-to-weight ratio)
     #  for every item from index 0 to n-1
-
     for i in range(n):
         for j in range(i+1,n):
             if(items[i][2] < items[j][2]):
